f89e1193373c5c8a70d689481bbbf4c0627e0bf2
Vadim Ignatov <ignatov0131@gmail.com>
2022-02-11 15:40:41 +0300

    Rework api methods

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,22 @@
-const privateKey = 'c406b82267824f2383593cd2bc30bf21';
+const privateKey = process.env.REACT_APP_PRIVATE_API_KEY;
+const gamesUrlAPI = process.env.REACT_APP_GAME_URL;
 
-export const getGameById = async (id) => {
-    const response = await fetch(`https://api.rawg.io/api/games/${id}?key=${privateKey}`);
+const requestToAPI = async (url) => {
+    const response = await fetch(`${gamesUrlAPI}${url}${url.indexOf('?') === -1 ? '?' : '&'}key=${privateKey}`)
 
     return response.json();
+}
+
+const getSearchParamsFromObject = (options) => {
+    const searchParams = [];
+
+    for (const [key, value] of Object.entries(options)) {
+        searchParams.push(`${key}=${value}`);
+    }
+
+    return searchParams.join('&');
+}
+
+export const getGameList = async (options = null) => {
+    return requestToAPI(`/games?${options ? getSearchParamsFromObject(options) : ''}`)
 }
