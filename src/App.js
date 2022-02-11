da80329808fb59551eae3ae6150c4d42761b37b0
Vadim Ignatov <ignatov0131@gmail.com>
2022-02-11 14:59:50 +0300

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
-import { useState } from "react";
-import { getGameById } from "./services/api";
-import GameCard from "./components/GameCard";
-import UIButton from "./components/UI/ui-button";
+import GetRandomGame from './pages/GetRandomGame';
 
-function App() {
-  const [gameCards, setGameCards] = useState([]);
-
-  const getRandomGame = async () => {
-    console.log('berba')
-    const maxId = 5000;
-    const game = await getGameById(Math.floor(Math.random() * maxId));
-
-    setGameCards([game, ...gameCards]);
-  }
-
+export default function App() {
   return (
     <div className="App">
-      <div className="random__container">
-        <div className="random__description">
-          <h1>Random Game Access</h1>
-          <p>
-            This is a game randomizer. If you don't now, what game is choose, use this app.
-          </p>
-
-          <p>
-            P.S. App use public API and making on React
-          </p>
-          <UIButton onClick={getRandomGame}>
-            Get random game!
-          </UIButton>
-        </div>
-        
-        <div className="games-container">
-          {
-            gameCards.length > 0 && gameCards.map((card, index) => 
-              <GameCard
-                key={index}
-                title={card.name}
-                releaseDate={card.released}
-                cover={card.background_image}
-                score={card.metacritic}
-              />
-          )}
-        </div>
-      </div>
+      <GetRandomGame />
     </div>
   );
 }
-
-export default App;
