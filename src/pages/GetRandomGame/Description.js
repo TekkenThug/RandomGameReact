9d3ec9217392b68e2bb4666ba10db7ae7759d58b
Vadim Ignatov <ignatov0131@gmail.com>
2022-02-11 15:00:33 +0300

    Add Description.js

---
+++ src/pages/GetRandomGame/Description.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Description() {
+    return (
+        <div>
+            <h1>Random Game Access</h1>
+            <p>
+                This is a game randomizer. If you don't now, what game is choose, use this app.
+            </p>
+
+            <p>
+                P.S. App use public API and making on React
+            </p>
+        </div>
+    )
+}
